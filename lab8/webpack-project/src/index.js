8321a9639315fcb10df1f22157a2bc21205405fa
wanderrlust <kuchmakivan@gmail.com>
2024-12-09 16:08:29 +0200

    lab8 is complete

---
+++ lab8/webpack-project/src/index.js
@@ -0,0 +1,8 @@
+import './styles.css';
+
+console.log('Webpack is working!');
+
+const app = document.createElement('div');
+app.className = 'app';
+app.textContent = 'Hello, Webpack!';
+document.body.appendChild(app);
